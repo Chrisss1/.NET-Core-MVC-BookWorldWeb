6540f074be1f016b8a7bc523c14c5dfa531ad21a
Kristóf Szénási <szenasi.kristof@outlook.com>
2023-03-05 20:44:56 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWorldWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWorldWeb.Data
+{
+	public class ApplicationDbContext: DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
